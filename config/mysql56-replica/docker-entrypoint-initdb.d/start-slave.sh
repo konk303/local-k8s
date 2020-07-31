0ba5abe943a7a2a196c103e8e32d95a1f9ba2f56
Kimihiko NAKAMURA <nakamura.808@gmail.com>
2020-07-31 19:23:19 +0900

    master -> primary

--- config/mysql56-replica/docker-entrypoint-initdb.d/start-slave.sh
+++ config/mysql56-replica/docker-entrypoint-initdb.d/start-slave.sh
@@ -3,27 +3,27 @@
 # Copied from https://qiita.com/takyam/items/f13bc4a1db0433ffb958
 
 # depends_onの設定しておけば気にならないけど念の為masterの起動を待つ
-while ! mysqladmin ping -h ${MYSQL56_MASTER_SVC_SERVICE_HOST} -P ${MYSQL56_MASTER_SVC_SERVICE_PORT} --silent; do
+while ! mysqladmin ping -h ${MYSQL56_PRIMARY_SVC_SERVICE_HOST} -P ${MYSQL56_PRIMARY_SVC_SERVICE_PORT} --silent; do
   sleep 1
 done
 
 # masterをロックする
-mysql -u root -h ${MYSQL56_MASTER_SVC_SERVICE_HOST} -P ${MYSQL56_MASTER_SVC_SERVICE_PORT} -e "RESET MASTER;"
-mysql -u root -h ${MYSQL56_MASTER_SVC_SERVICE_HOST} -P ${MYSQL56_MASTER_SVC_SERVICE_PORT} -e "FLUSH TABLES WITH READ LOCK;"
+mysql -u root -h ${MYSQL56_PRIMARY_SVC_SERVICE_HOST} -P ${MYSQL56_PRIMARY_SVC_SERVICE_PORT} -e "RESET MASTER;"
+mysql -u root -h ${MYSQL56_PRIMARY_SVC_SERVICE_HOST} -P ${MYSQL56_PRIMARY_SVC_SERVICE_PORT} -e "FLUSH TABLES WITH READ LOCK;"
 
 # masterのDB情報をDumpする
 # ここでは --all-databases にしてるけど用途に応じて必要なDBだけにしていいと思う
-mysqldump -uroot -h ${MYSQL56_MASTER_SVC_SERVICE_HOST} -P ${MYSQL56_MASTER_SVC_SERVICE_PORT} --all-databases --master-data --single-transaction --flush-logs --events > /tmp/master_dump.sql
+mysqldump -uroot -h ${MYSQL56_PRIMARY_SVC_SERVICE_HOST} -P ${MYSQL56_PRIMARY_SVC_SERVICE_PORT} --all-databases --master-data --single-transaction --flush-logs --events > /tmp/master_dump.sql
 # 特定のDBだけにする場合はこんな感じ(my.cnfのreplica-do-dbも忘れずに設定すること)
-# mysqldump -uroot -h ${MYSQL56_MASTER_SVC_SERVICE_HOST} -P ${MYSQL56_MASTER_SVC_SERVICE_PORT} データベース名 --master-data --single-transaction --flush-logs --events > /tmp/master_dump.sql
+# mysqldump -uroot -h ${MYSQL56_PRIMARY_SVC_SERVICE_HOST} -P ${MYSQL56_PRIMARY_SVC_SERVICE_PORT} データベース名 --master-data --single-transaction --flush-logs --events > /tmp/master_dump.sql
 
 # dumpしたmasterのDBをslaveにimportする
 mysql -u root -e "STOP SLAVE;";
 mysql -u root < /tmp/master_dump.sql
 
 # masterに繋いで bin-logのファイル名とポジションを取得する
-log_file=`mysql -u root -h ${MYSQL56_MASTER_SVC_SERVICE_HOST} -P ${MYSQL56_MASTER_SVC_SERVICE_PORT} -e "SHOW MASTER STATUS\G" | grep File: | awk '{print $2}'`
-pos=`mysql -u root -h ${MYSQL56_MASTER_SVC_SERVICE_HOST} -P ${MYSQL56_MASTER_SVC_SERVICE_PORT} -e "SHOW MASTER STATUS\G" | grep Position: | awk '{print $2}'`
+log_file=`mysql -u root -h ${MYSQL56_PRIMARY_SVC_SERVICE_HOST} -P ${MYSQL56_PRIMARY_SVC_SERVICE_PORT} -e "SHOW MASTER STATUS\G" | grep File: | awk '{print $2}'`
+pos=`mysql -u root -h ${MYSQL56_PRIMARY_SVC_SERVICE_HOST} -P ${MYSQL56_PRIMARY_SVC_SERVICE_PORT} -e "SHOW MASTER STATUS\G" | grep Position: | awk '{print $2}'`
 
 # slaveの開始
 mysql -u root -e "RESET SLAVE";
@@ -31,4 +31,4 @@ mysql -u root -e "CHANGE MASTER TO MASTER_HOST='mysql', MASTER_USER='root', MAST
 mysql -u root -e "start slave"
 
 # masterをunlockする
-mysql -u root -h ${MYSQL56_MASTER_SVC_SERVICE_HOST} -P ${MYSQL56_MASTER_SVC_SERVICE_PORT} -e "UNLOCK TABLES;"
+mysql -u root -h ${MYSQL56_PRIMARY_SVC_SERVICE_HOST} -P ${MYSQL56_PRIMARY_SVC_SERVICE_PORT} -e "UNLOCK TABLES;"
